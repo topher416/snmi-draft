b8b820bbd21646fc0027f5d69628bc7ec8cd7407
topher416 <topher416@gmail.com>
2025-04-24 22:30:24 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/snmi-draft' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/snmi-draft/' : '',
 }
 
 export default nextConfig
